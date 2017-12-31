37ba9b2731d6df2eb39ece08d061481510ec0bd7
Alice Rixte <alice.rixte@gmail.com>
2017-12-31 18:47:39 +0100

    object bag (not tested yet)

--- src/state.ml
+++ src/state.ml
@@ -131,36 +131,7 @@ let open_game json_file =
 (* *********************** End parsing ********************************* *)
 
 
-(* *********************** Bag gestion ********************************* *)
-(* creation of the bag*)
-let distrib = [('_',2);('A',9);('B',2);('C',2);('D',3);('E',15);('F',2);
-               ('G',2);('H',2);('I',8);('J',1);('K',1);('L',5);('M',3);
-               ('N',6);('O',6);('P',2);('Q',1);('R',6);('S',6);('T',6);
-               ('U',6);('V',2);('W',1);('X',1);('Y',1);('Z',1)]
-
-let bag_list = ref []
-
-let add_letter c =
-  bag_list := !bag_list @ Array.to_list (Array.make (snd c) (fst c))
-
-(*creation of a new random bag*)
-let new_bag () =
-  List.iter add_letter distrib;
-  Random.self_init ();
-  let tmp = List.map (fun c -> (Random.bits (), c)) !bag_list in
-  let sorted = List.sort compare tmp in
-  bag_list := List.map snd sorted
-
-(*Piocher n lettres*)
-let pick_letters n =
-  try (*si on a assez de lettres*)
-    let ret = String.sub !bag 0 n in
-    bag := String.sub !bag n (String.length !bag - n);
-    ret
-  with (*sinon*)
-    _ -> let ret = !bag in
-        bag := "";
-        ret
+let bag = new Bag.bag Bag.fr_distrib
 
 let empty_board () =
   for i = 0 to Array.length board - 1 do
@@ -176,7 +147,7 @@ let new_game names =
   players := Array.make nb_names default_player;
   for i = 0 to - 1 do
     (!players).(i) <- new Player.humanPlayer
-                       names.(i) 0 (pick_letters Bag.max_nb_letters)
+                       names.(i) 0 (bag#pick_letters Bag.max_nb_letters)
   done;
   empty_board ()
 
