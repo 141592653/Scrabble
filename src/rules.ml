31c147bb35a25e71e919707e27c1a67e432a9146
Alice Rixte <alice.rixte@gmail.com>
2018-01-25 15:33:17 +0100

    rules

---
+++ src/rules.ml
@@ -0,0 +1,52 @@
+type orientation = H|V
+let int_of_orientation o = match o with
+  |H -> 0
+  |V -> 1
+		       
+let max_nb_letters = 7
+
+(* *********************** Bag gestion ********************************* *)
+
+type letter_distrib = (char*int) list
+			       
+(* creation of the bag*)
+let fr_distrib = [('_',2);('A',9);('B',2);('C',2);('D',3);('E',15);('F',2);
+               ('G',2);('H',2);('I',8);('J',1);('K',1);('L',5);('M',3);
+               ('N',6);('O',6);('P',2);('Q',1);('R',6);('S',6);('T',6);
+               ('U',6);('V',2);('W',1);('X',1);('Y',1);('Z',1)]
+
+
+(*conversion of a distribution to a randomly shuffled string*)
+let string_of_distrib distrib =
+   let bag_list = ref [] in
+   let add_letter c =
+    bag_list := !bag_list @ Array.to_list (Array.make (snd c) (fst c));
+   in
+   List.iter add_letter distrib; (*now we've got the list of char not shuffled*)
+   (*let'sshuffle it !*)
+   Random.self_init ();
+   let tmp = List.map (fun c -> (Random.bits (), c)) !bag_list in
+   let sorted = List.sort compare tmp in
+   (*now we've got a shuffled list of char*)
+   (*let's transform it into a string*)
+   bag_list := List.map snd sorted;
+   String.concat "" (List.map Char.escaped !bag_list)
+		 
+		 
+class bag (distrib : letter_distrib) = 
+  let bag_a = string_of_distrib distrib in 
+  object
+    val mutable bag_str = bag_a
+			    
+    (*Piocher n lettres*)
+    method pick_letters n =
+      try (*si on a assez de lettres*)
+	let ret = String.sub bag_str 0 n in
+	bag_str <- String.sub bag_str n (String.length bag_str - n);
+	ret
+      with (*sinon*)
+	_ -> let ret = bag_str in
+             bag_str <- "";
+             ret
+
+  end
